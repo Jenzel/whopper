15f9e61cf64c0d71b830719838cdde2102522836
Lars <Lars@192.168.32.156>
2012-02-28 12:22:04 +0100

    HELEMAAL SCHONE VERSIE OMG!

--- Whopper/main.cpp
+++ Whopper/main.cpp
@@ -1,13 +1,6 @@
 #include <ma.h>
 #include <conprint.h>
 
-/**
- * Entry point of the program. The MAMain function
- * needs to be declared as extern "C".
- *
- * Create a Moblet project to use the high level
- * MoSync C++ library.
- */
 extern "C" int MAMain()
 {
 	MAEvent event;
@@ -21,14 +14,13 @@ extern "C" int MAMain()
 
 		if (EVENT_TYPE_CLOSE == event.type)
 		{
-			// Exit while loop.
+
 			break;
 		}
 		else if (EVENT_TYPE_KEY_PRESSED == event.type)
 		{
 			if (MAK_BACK == event.key || MAK_0 == event.key)
 			{
-				// Exit while loop.
 				break;
 			}
 
