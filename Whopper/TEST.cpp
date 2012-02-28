4b25ca9e29f47a2460e84ee503b1e9d860bfc91f
Lars <Lars@192.168.32.156>
2012-02-28 11:43:21 +0100

    TEST

---
+++ Whopper/TEST.cpp
@@ -0,0 +1,18 @@
+/*
+ * TEST.cpp
+ *
+ *  Created on: 28 feb. 2012
+ *      Author: Lars
+ */
+
+#include "TEST.h"
+
+TEST::TEST() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TEST::~TEST() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Whopper/TEST.h
@@ -0,0 +1,17 @@
+/*
+ * TEST.h
+ *
+ *  Created on: 28 feb. 2012
+ *      Author: Lars
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+class TEST {
+public:
+	TEST();
+	virtual ~TEST();
+};
+
+#endif /* TEST_H_ */
